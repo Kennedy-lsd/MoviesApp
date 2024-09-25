09f902921c4e4182fbf7152377f9b262166b4717
Kennedy <santes1263@gmail.com>
2024-09-26 00:56:04 +0300

    Update User controller and router

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,42 +2,108 @@ import { UserTable } from "../schemas/user";
 import { db } from "../drizzle/db";
 import { Request, Response } from "express";
 import { eq } from "drizzle-orm";
+import userTableValidator from "../utils/validatorSchemas/userTableValidator";
+import { UserData } from "../utils/types/types";
 
 const getAllUsers = async (req: Request, res: Response) => {
-    try {
-        const result = await db.select().from(UserTable)
+  try {
+    const result = await db.select().from(UserTable);
 
-        if (!result.length) {
-            return res.status(404).json({message: "Not found"})
-          }
-          res.status(200).json(result);
-
-    } catch (error: any) {
-        console.error(error)
-        res.status(500).json({message: "Server error"})
+    if (!result.length) {
+      return res.status(404).json({ message: "Not found" });
     }
-}
+    res.status(200).json(result);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 const getOneUser = async (req: Request, res: Response) => {
-    const { id } = req.params
+  const { id } = req.params as { id: string };
 
-    if (!id) {
-        return res.status(400).json({message: "Invalid user id"})
+  if (!id) {
+    return res.status(400).json({ message: "Invalid user id" });
+  }
+
+  try {
+    const result = await db
+      .select()
+      .from(UserTable)
+      .where(eq(UserTable.id, id));
+    if (!result.length) {
+      return res.status(404).json({ message: "Not found" });
     }
+    res.status(200).json(result[0]);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-    try {
-        const result = await db.select().from(UserTable).where(eq(UserTable.id, id))
-        if (!result.length) {
-            return res.status(404).json({message: "Not found"})
-          }
-          res.status(200).json(result);
-    } catch (error: any) {
-        console.error(error)
-        res.status(500).json({message: "Server error"})
+const createUser = async (req: Request, res: Response) => {
+  const userData: UserData = req.body;
+
+  const validationResult = userTableValidator.safeParse(userData);
+
+  if (!validationResult.success) {
+    return res.status(400).json({
+      error: "Validation failed",
+      details: validationResult.error.errors,
+    });
+  }
+
+  try {
+    const validatedData = validationResult.data;
+
+    const newUser = await db
+      .insert(UserTable)
+      .values(validatedData)
+      .returning();
+
+    res.status(200).json(newUser[0]);
+  } catch (error: any) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deleteUser = async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+  try {
+    const deletedUser = await db
+      .delete(UserTable)
+      .where(eq(UserTable.id, id))
+      .returning();
+
+    if (!deletedUser.length) {
+      return res.status(404).json({ message: "Not found" });
     }
-}
+    res.status(200).json({ message: "Successfully delete" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
-export {
-    getAllUsers,
-    getOneUser
-}
+const updateUser = async (req: Request, res: Response) => {
+  const { id } = req.params as { id: string };
+  const data: UserData = req.body;
+  try {
+    const updatedUser = await db
+      .update(UserTable)
+      .set(data)
+      .where(eq(UserTable.id, id))
+      .returning();
+
+    if (!updatedUser.length) {
+      return res.status(404).json({ message: "Not found" });
+    }
+    res.status(200).json(updatedUser[0]);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json("Server error");
+  }
+};
+
+export { getAllUsers, getOneUser, createUser, deleteUser, updateUser };
--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { getAllUsers, getOneUser } from "../controllers/userController";
+import { getAllUsers, getOneUser, createUser, deleteUser, updateUser } from "../controllers/userController";
+import { handleRequest } from "../utils/middlewares/uuidHandler";
 
 const router = Router()
 
 router.get("/api/users", getAllUsers)
 
-router.get("/api/users/:id", getOneUser)
+router.post("/api/users", createUser)
+
+router.get("/api/users/:id", handleRequest, getOneUser)
+
+router.delete("/api/users/:id", handleRequest, deleteUser)
+
+router.patch("/api/users/:id", handleRequest, updateUser)
 
 export default router
