737ab32952547067348d765d1b6989f0c148471b
Kennedy <santes1263@gmail.com>
2024-09-26 00:55:33 +0300

    Update packages and create some types

---
+++ src/utils/types/types.ts
@@ -0,0 +1,12 @@
+export type UserData = {
+    image: string
+    email: string
+    username: string
+    password: string
+}
+
+export type VideoType = {
+    title: string
+    url: string
+    description: string
+}
